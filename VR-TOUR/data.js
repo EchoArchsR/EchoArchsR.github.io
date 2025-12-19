308f0e7043360fc72034f0aa4968f477a2a660a2
GM-WH <gdaud175@gmail.com>
2025-12-19 08:24:42 +0300

    Sub

--- VR-TOUR/data.js
+++ VR-TOUR/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-secretary-area-view-a",
-      "name": "Secretary Area View A",
+      "id": "0-secretary-view-a",
+      "name": "Secretary View A",
       "levels": [
         {
           "tileSize": 256,
@@ -32,8 +32,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "1-secretary-area-view-b",
-      "name": "Secretary Area View B",
+      "id": "1-secretary-view-b",
+      "name": "Secretary View B",
       "levels": [
         {
           "tileSize": 256,
@@ -63,69 +63,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "2-waiting-room-view-a",
-      "name": "Waiting Room View A",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-waiting-room-view-b",
-      "name": "Waiting Room View B",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-ceo-office-view-a",
+      "id": "2-ceo-office-view-a",
       "name": "CEO Office View A",
       "levels": [
         {
@@ -156,7 +94,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "5-ceo-office-view-b",
+      "id": "3-ceo-office-view-b",
       "name": "CEO Office View B",
       "levels": [
         {
@@ -187,7 +125,7 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "6-ceo-office-view-c",
+      "id": "4-ceo-office-view-c",
       "name": "CEO Office View C",
       "levels": [
         {
@@ -218,8 +156,70 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "7-ceo-office-view-d",
+      "id": "5-ceo-office-view-d",
       "name": "CEO Office View D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-waiting-room-view-a",
+      "name": "Waiting Room View A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-waiting-room-view-b",
+      "name": "Waiting Room View B",
       "levels": [
         {
           "tileSize": 256,
